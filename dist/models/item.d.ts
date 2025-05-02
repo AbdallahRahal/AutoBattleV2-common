dcd120fd302b9b1d4a44496a45df258291f39dac
Abdallah <abdallah@milango.io>
2025-05-02 12:24:07 +0200

    changed item

--- dist/models/item.d.ts
+++ dist/models/item.d.ts
@@ -1,9 +1,7 @@
 import { StatRecord } from "./stat";
 export interface Item {
-    updatedAt?: Date;
     name: string;
     description: string;
-    level: number;
     stats?: Partial<StatRecord>;
     spellId?: number;
     icon: string;
--- src/models/item.ts
+++ src/models/item.ts
@@ -1,10 +1,8 @@
 import { StatRecord } from "./stat";
 
 export interface Item {
-    updatedAt?: Date;
     name: string,
     description: string,
-    level: number,
     stats?: Partial<StatRecord>,
     spellId?: number
     icon: string
