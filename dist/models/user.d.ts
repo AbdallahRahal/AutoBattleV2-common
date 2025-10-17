54c349ccae78a77530439ef359f26e5363f23fda
Abdallah <abdallah.rahal15@gmail.com>
2025-10-17 16:08:57 +0200

    add accountStatId

--- dist/models/user.d.ts
+++ dist/models/user.d.ts
@@ -1,3 +1,4 @@
+import { ClassName } from "./character";
 export interface User {
     id: string;
     discordId: string;
@@ -8,5 +9,16 @@ export interface User {
     randomModelCoin: number;
     weaponSpellId: number[];
     relicId: string;
+    accountStats: UserAccountStats;
     lastModelSelected: string;
+}
+export interface UserAccountStats {
+    charactersCreated: number;
+    tournamentsWon: number;
+    raidsWon: number;
+    raidsPlayed: number;
+    duelsWon: number;
+    duelsPlayed: number;
+    merchantsKilled: number;
+    classePlayed: Record<ClassName, number>;
 }
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,3 +1,4 @@
+import { ClassName } from "./character";
 
 export interface User {
     id: string;
@@ -15,6 +16,25 @@ export interface User {
     weaponSpellId: number[];
     relicId: string
 
+    accountStats: UserAccountStats
+
     lastModelSelected: string;
 }
 
+export interface UserAccountStats {
+
+    charactersCreated: number;
+
+    tournamentsWon: number;
+
+    raidsWon: number;
+    raidsPlayed: number
+
+    duelsWon: number;
+    duelsPlayed: number
+
+    merchantsKilled: number;
+
+    classePlayed: Record<ClassName, number>
+
+}
