8fc69453512e10f8110f991709b375d92fb22d39
Abdallah <abdallah.rahal15@gmail.com>
2026-02-22 12:48:26 +0100

    added ApplicationType and school to damage and heal context

--- dist/models/index.js
+++ dist/models/index.js
@@ -34,3 +34,4 @@ __exportStar(require("./status"), exports);
 __exportStar(require("./defi"), exports);
 __exportStar(require("./classItemRules"), exports);
 __exportStar(require("./vote"), exports);
+__exportStar(require("./context"), exports);
---
+++ src/models/context.ts
@@ -0,0 +1,10 @@
+export enum ApplicationType {
+    Direct = "Direct",
+    Indirect = "Indirect",
+}
+
+export enum School {
+    Physical = "Physical",
+    Magical = "Magical",
+
+}
