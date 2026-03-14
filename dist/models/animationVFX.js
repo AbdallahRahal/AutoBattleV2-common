31b9eecc9b87d4224c29114ef692586d621daf72
Abdallah <abdallah.rahal15@gmail.com>
2026-03-14 16:08:41 +0100

    added animation

--- dist/models/animationVFX.js
+++ dist/models/animationVFX.js
@@ -4,10 +4,5 @@ exports.AnimationVFX = void 0;
 var AnimationVFX;
 (function (AnimationVFX) {
     AnimationVFX["FIREBALL"] = "FIREBALL";
-    AnimationVFX["FIREBALL_AURA"] = "FIREBALL_AURA";
-    AnimationVFX["SLASH_HEAVY"] = "SLASH_HEAVY";
-    AnimationVFX["DOUBLE_SLASH"] = "DOUBLE_SLASH";
-    AnimationVFX["HOLY_STRIKE"] = "HOLY_STRIKE";
-    AnimationVFX["HEAL_GLOW"] = "HEAL_GLOW";
-    AnimationVFX["SUMMON_FLASH"] = "SUMMON_FLASH";
+    AnimationVFX["BRULURE_SACREE"] = "BRULURE_SACREE";
 })(AnimationVFX || (exports.AnimationVFX = AnimationVFX = {}));
