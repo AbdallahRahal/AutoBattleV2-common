42908e2af878924079003ddbfe4ef10cc6e9608b
Abdallah <abdallah.rahal15@gmail.com>
2026-03-15 17:37:13 +0100

    added INVOCATION_ARCHER_ATTAQUE to AnimationVFX

--- dist/models/animationVFX.js
+++ dist/models/animationVFX.js
@@ -6,4 +6,5 @@ var AnimationVFX;
     AnimationVFX["FIREBALL"] = "FIREBALL";
     AnimationVFX["BRULURE_SACREE"] = "BRULURE_SACREE";
     AnimationVFX["TIR_MORTEL"] = "TIR_MORTEL";
+    AnimationVFX["INVOCATION_ARCHER_ATTAQUE"] = "INVOCATION_ARCHER_ATTAQUE";
 })(AnimationVFX || (exports.AnimationVFX = AnimationVFX = {}));
