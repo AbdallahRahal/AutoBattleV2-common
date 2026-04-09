6d78f63f185e792098c7c911a9ffcd4d0b1e4933
Abdallah <abdallah.rahal15@gmail.com>
2026-04-10 01:26:03 +0200

    added adventure item random generation

--- src/models/adventureItem.ts
+++ src/models/adventureItem.ts
@@ -2,6 +2,10 @@ import { Item } from './item';
 
 export type AdventureSlot = 'chest' | 'head' | 'ring';
 
+export type AdventureItemRarity = 'common' | 'rare' | 'epic' | 'legendary';
+
 export interface AdventureItem extends Item {
     slot: AdventureSlot;
+    rarity: AdventureItemRarity;
+    level: number;
 }
